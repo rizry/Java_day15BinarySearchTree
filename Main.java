0755b274bec337a6b9cb4f3bc9c3adee576e2d21
rizry <rizzwan.ansarii@gmail.com>
2022-10-02 16:11:38 +0530

    added multiple nodes to binary tree

--- Main.java
+++ Main.java
@@ -7,7 +7,17 @@ public class Main {
     myBinaryTree.add(56);
     myBinaryTree.add(30);
     myBinaryTree.add(70);
-
+    myBinaryTree.add(40);
+    myBinaryTree.add(22);
+    myBinaryTree.add(11);
+    myBinaryTree.add(3);
+    myBinaryTree.add(16);
+    myBinaryTree.add(60);
+    myBinaryTree.add(95);
+    myBinaryTree.add(65);
+    myBinaryTree.add(63);
+    myBinaryTree.add(67);
+    
     int size = myBinaryTree.getSize();
     System.out.println("size = " + size);
   }
